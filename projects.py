02564a5f87d9436882222830500fd716a1c48e42
NAKARABOYINA RAM KUMAR <nakhraboinaramkumar@gmail.com>
2025-12-25 09:00:15 +0530

    Python mini projects

---
+++ projects.py
@@ -0,0 +1,16 @@
+# Number Guessing Game
+# import random
+# random_num=random.randint(1,10)
+# name=input("Enter your name: ")
+# print(f"Hello {name.capitalize()}\nWelcome to the NUMBER GUESSING GAME")
+# for i in range(3):
+#     num=int(input("Guess the number between 1 to 10: "))
+#     if num==random_num:
+#         print("congratulations")
+#         break
+#     elif num>random_num:
+#         print("too high")
+#     elif num<random_num:
+#         print("too low")
+#     else:
+#         print("sorry, you guess wrong number")
